6b3f1b02fddb71c24b34c37843b524b03e7c7db0
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-05-30 19:10:42 +0300

    indent node sets

--- src/rules.rs
+++ src/rules.rs
@@ -38,6 +38,9 @@ pub(crate) fn spacing() -> Vec<dsl::SpacingRule> {
     r(inside(NODE_LIST).after(T!['[']).single_space_or_newline());
     r(inside(NODE_LIST).before(T![']']).single_space_or_newline());
 
+    r(inside(NODE_SET).after(T!['{']).single_space_or_newline());
+    r(inside(NODE_SET).before(T!['}']).single_space_or_newline());
+
     rules
 }
 
@@ -46,6 +49,7 @@ pub(crate) fn indentation() -> Vec<dsl::IndentRule> {
     let mut rules = Vec::new();
     let mut r = |i: dsl::IndentRule| rules.push(i);
     r(indent(NODE_LIST, LIST_ELEMENTS));
+    r(indent(NODE_SET, NODE_SET_ENTRY));
     rules
 }
 
@@ -68,6 +72,16 @@ mod tests {
     };
 
     use crate::reformat_string;
+
+    #[test]
+    fn smoke() {
+        TestCase {
+            name: None,
+            before: "{ foo = 1;\nbar = 2; }".into(),
+            after: "{\n  foo = 1;\n  bar = 2;\n}".into(),
+        }
+        .run();
+    }
 
     /// For convenience, tests in this module are specified inline in comments,
     /// right next to the corresponding rule definition. This test looks at the
@@ -140,20 +154,18 @@ mod tests {
             assert!(res.len() > 0);
             res
         }
-    }
 
-    fn run(tests: &[TestCase]) {
-        for test_case in tests {
-            let name = test_case.name.as_ref().map(|it| it.as_str()).unwrap_or("");
-            let expected = &test_case.after;
-            let actual = &reformat_string(&test_case.before);
+        fn run(&self) {
+            let name = self.name.as_ref().map(|it| it.as_str()).unwrap_or("");
+            let expected = &self.after;
+            let actual = &reformat_string(&self.before);
             if expected != actual {
                 panic!(
                     "assertion failed({}): wrong formatting\
                      \nBefore:\n{}\n\
                      \nAfter:\n{}\n\
                      \nExpected:\n{}\n",
-                    name, test_case.before, actual, test_case.after,
+                    name, self.before, actual, self.after,
                 )
             }
             let second_round = &reformat_string(actual);
@@ -166,5 +178,9 @@ mod tests {
                 )
             }
         }
+    }
+
+    fn run(tests: &[TestCase]) {
+        tests.iter().for_each(|it| it.run())
     }
 }
