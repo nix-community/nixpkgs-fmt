ef06d64a33e6037aa8d5579013bdcd3530531d7e
zimbatm <zimbatm@zimbatm.com>
2019-06-06 14:16:32 +0200

    test: condition tests

---
+++ test_data/cond.bad.nix
@@ -0,0 +1,13 @@
+{
+  singleline = xxx: if true then true else false
+    ;
+
+  multiline = xxx:
+    if
+    true
+    then
+    true
+    else
+    false
+    ;
+}
---
+++ test_data/cond.good.nix
@@ -0,0 +1,12 @@
+{
+  singleline = xxx:
+    if true then true else false
+    ;
+
+  multiline = xxx:
+    if true then
+      true
+    else
+      false
+    ;
+}
