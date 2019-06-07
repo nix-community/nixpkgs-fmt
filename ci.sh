941b2af6fd11f604bbb54c5a07c3f0866cb23c2d
zimbatm <zimbatm@zimbatm.com>
2019-06-07 22:22:48 +0200

    ci: build with nix

---
+++ ci.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash
+#
+# This is the script executed by CI
+set -euo pipefail
+
+## Functions ##
+
+run() {
+  echo >&2
+  echo "$ ${*@Q}" >&2
+  "$@"
+}
+
+## Main ##
+
+mkdir -p "${TMPDIR}"
+
+run cargo build --verbose
+
+run ./wasm/build.sh
+
+run cargo test --verbose
