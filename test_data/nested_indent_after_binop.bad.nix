5211b39dfac70f8cb8de0b4b627329a8d16085fc
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-06-17 18:22:12 +0300

    indent binops relative to right hand side

---
+++ test_data/nested_indent_after_binop.bad.nix
@@ -0,0 +1,6 @@
+pkgs.example rec {
+  buildInputs = [ pkgs.foo  ] ++
+    pkgs.stdenv.lib.optionals pkgs.stdenv.isDarwin [
+      pkgs.bar
+    ];
+}
---
+++ test_data/nested_indent_after_binop.good.nix
@@ -0,0 +1,7 @@
+pkgs.example rec {
+  buildInputs = [ pkgs.foo ]
+    ++ pkgs.stdenv.lib.optionals pkgs.stdenv.isDarwin [
+         pkgs.bar
+       ]
+  ;
+}
