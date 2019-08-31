7f68a3044bf4a5656c7022f0ed9f3937955e2ab3
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-08-31 11:18:07 +0300

    Don't force newline before ++

--- test_data/nested_indent_after_binop.good.nix
+++ test_data/nested_indent_after_binop.good.nix
@@ -1,7 +1,5 @@
 pkgs.example rec {
-  buildInputs = [ pkgs.foo ]
-    ++ pkgs.stdenv.lib.optionals pkgs.stdenv.isDarwin [
-         pkgs.bar
-       ]
-    ;
+  buildInputs = [ pkgs.foo ] ++ pkgs.stdenv.lib.optionals pkgs.stdenv.isDarwin [
+    pkgs.bar
+  ];
 }
