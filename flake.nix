ed5928b79673249fc3f850a29bd5f57fbf02007f
zimbatm <zimbatm@zimbatm.com>
2020-07-07 14:44:03 +0200

    update flake to the new format

--- flake.nix
+++ flake.nix
@@ -1,17 +1,12 @@
 {
-  description = "A flake for building Hello World";
+  description = "nixpkgs-fmt flake";
 
-  edition = 201909;
-
-  inputs.naersk = { uri = "github:nmattia/naersk"; flake = false; };
+  inputs.naersk = { url = "github:nmattia/naersk"; };
 
   outputs = { self, nixpkgs, naersk }:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-      naerskOverlay = final: prev: {
-        naersk = final.callPackage naersk { };
-      };
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems f;
     in
     {
       overlay = final: prev: {
@@ -25,7 +20,7 @@
             nixpkgs-fmt = (import nixpkgs {
               inherit system;
               overlays = [
-                naerskOverlay
+                naersk.overlay
                 self.overlay
               ];
             }).nixpkgs-fmt;
