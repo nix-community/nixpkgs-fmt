90be3a6b931c1083572a1fb090b28f6ea5de7e60
rizary <andika.riyan@gmail.com>
2021-03-18 21:26:27 +0700

    add and fix new test_date to match the new block comment formatting

--- test_data/reindents_block_comments.bad.nix
+++ test_data/reindents_block_comments.bad.nix
@@ -1,8 +1,17 @@
 {
-/*
- * decoreted
- * block
- * comment
- */
+/* 
+decoreted
+block
+comment
+*/
 foo = 92;
+    /* the closing block comment
+       should not in the newline */
+  toPretty = {
+    /* the closing block comment
+       should be aligned with the opening block comment
+       */
+      allowPrettyValues ? false
+    }@args:
+    let a = 0; in a;
 }
