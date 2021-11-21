2dc351c7012441f23d14873a7a994abe58fa6bc7
zimbatm <zimbatm@zimbatm.com>
2021-11-21 14:56:30 +0100

    nix: remove naersk
    Just to collapse the dependency tree.

--- flake.nix
+++ flake.nix
@@ -2,12 +2,10 @@
   description = "nixpkgs-fmt";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.naersk.url = "github:nmattia/naersk";
-  inputs.naersk.inputs.nixpkgs.follows = "nixpkgs";
   inputs.fenix.url = "github:nix-community/fenix";
   inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, naersk, fenix, flake-utils }@inputs:
+  outputs = { self, nixpkgs, fenix, flake-utils }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import ./. { inherit system inputs; }; in
       {
