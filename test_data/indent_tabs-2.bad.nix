41c085887259c8744f225a1a573b8cb5b4f8a50b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-11-22 19:27:41 +0100

    Add tab character tests

---
+++ test_data/indent_tabs-2.bad.nix
@@ -0,0 +1,25 @@
+args	@	{	z	,	}	:
+
+		  	{
+		a	 =  	 	"1"	;
+		b	 =	 {	} 	// 	{	}	;		
+		c 	=	   	z	.	y	;
+		d 	=	 1.2	;	
+	e 	=	   5	  	;
+		f	 = 	./test		;
+			g	 = 	true	;		
+		h 		=	 null	;
+	i 	  		=	 [	 1	 ./example.bin	 { 		hello	=		"world"	;	 } 	]	;	
+			j 	= 		5	 * 			5;			
+	k     = z ?	 		fs		    ;
+	l   = z 		++		 z;
+	m 	=	 -	 		  324		;
+	n = z		  z    ;	
+	o = import 	 	 	 		<nixpkgs>		;
+	p = with  	 	 	  z	 	 ;	 	 	  "fdsfs";			    
+	q = ''
+  ${   			5		   +		   5  		}
+	'';
+	r = " f${  					z		 	 	    }ds ";
+		  	  		 }   
+			
