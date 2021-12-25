22ca69e938b6225898e91ba8de4495a548de2644
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2021-12-26 02:58:57 +0800

    test_data: add rtl_attact_*

---
+++ test_data/rtl_attact_comment_unclosed_good.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Unicode bidi (RTL) control characters
+# See https://www.unicode.org/reports/tr9/tr9-42.html
+UCHAR_LRE="$(printf "\\u202a")" 
+UCHAR_RLE="$(printf "\\u202b")"
+UCHAR_LRO="$(printf "\\u202d")"
+UCHAR_RLO="$(printf "\\u202e")"
+UCHAR_PDF="$(printf "\\u202c")"
+UCHAR_LRI="$(printf "\\u2066")"
+UCHAR_RLI="$(printf "\\u2067")"
+UCHAR_FSI="$(printf "\\u2068")"
+UCHAR_PDI="$(printf "\\u2069")"
+
+cat << EOF
+{ lib, hello }:
+{
+  hello-insecure = hello.overrideAttrs (oldAttrs: {
+    meta = oldAttrs.meta // {
+      /* Mark as insecure${UCHAR_RLO}${UCHAR_LRI} insecure = true;${UCHAR_PDI}${UCHAR_PDF} */
+    };
+  });
+}
+EOF
