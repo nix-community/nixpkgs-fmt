7a8e061ec4635a0a2c5a32d5176012a15e6d481d
Matthew Toohey <contact@mtoohey.com>
2022-07-08 23:23:08 -0400

    docs: explain ExtraInfo::Edits

--- src/engine.rs
+++ src/engine.rs
@@ -18,6 +18,8 @@ use crate::{
 
 pub(crate) enum ExtraInfo<'a> {
     Explanation(&'a mut Vec<(AtomEdit, Option<RuleName>)>),
+    /// Edits contains spacing edits to be applied to the document and indent edits to be applied in
+    /// a second, separate transaction.
     Edits { spacing_edits: &'a mut Vec<AtomEdit>, indent_edits: &'a mut Vec<AtomEdit> },
     None,
 }
